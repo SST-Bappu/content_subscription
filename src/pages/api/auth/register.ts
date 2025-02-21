a4c318cd7e3b8afbf195578b286bd0a20e752141
Satta Sundar Talukdar <bappu.ctg@gmail.com>
2025-02-21 16:26:28 +0600

    feat: add zod validation in registration api

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -1,26 +1,25 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { AuthService } from '@/services/authService';
-import { RegisterUserInput } from '@/interfaces/auth.interface';
+import {NextApiRequest, NextApiResponse} from 'next';
+import {AuthService} from '@/services/authService';
 import {RegisterRequestBody, RegisterSchema} from "@/dtos/auth.dtos";
 import {formatZodErrors} from "@/utils/zod.error.validator";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
-        return res.status(405).json({ error: "Method not allowed" });
+        return res.status(405).json({error: "Method not allowed"});
     }
 
     try {
         // Validate Request Body Using `zod`
         const validation = RegisterSchema.safeParse(req.body)
-        if (!validation.success){
+        if (!validation.success) {
             return res.status(400).json({error: formatZodErrors(validation.error)});
         }
         // Extract Validated Data
         const userData: RegisterRequestBody = validation.data
-        const { status, data } = await AuthService.register(userData);
+        const {status, data} = await AuthService.register(userData);
         return res.status(status).json(data);
     } catch (error) {
         console.error("Registration Error:", error);
-        return res.status(500).json({ error: "Internal Server Error" });
+        return res.status(500).json({error: "Internal Server Error"});
     }
 }
