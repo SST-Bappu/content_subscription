2b3b382aa02978450052afa0595e5c90d77c5d7a
Satta Sundar Talukdar <bappu.ctg@gmail.com>
2025-02-23 00:02:28 +0600

    fix: fix authentication middleware wrapper in categories and content handler

--- src/pages/api/subscriptions/categories.ts
+++ src/pages/api/subscriptions/categories.ts
@@ -1,20 +1,18 @@
 import {NextApiResponse} from 'next';
-import {authenticate} from '@/middleware/authMiddleware';
 import {AuthenticatedRequest} from "@/interfaces/auth.interface";
 import {categoryService} from "@/services/containers";
 import {errorHandler} from "@/middleware/errorHandlingMiddleware";
+import {authenticate} from "@/middleware/authMiddleware";
 
 async function handler(req: AuthenticatedRequest, res: NextApiResponse) {
     if (req.method !== 'GET') {
         throw {status: 405, message: "Method not allowed"};
     }
 
-    authenticate(req, res, async () => {
 
-        const {status, data} = await categoryService.getAllCategories();
-        return res.status(status).json({success: true, data: data});
-    });
+    const {status, data} = await categoryService.getAllCategories();
+    return res.status(status).json({success: true, data: data});
 
 }
 
-export default errorHandler(handler)
+export default errorHandler(authenticate(handler))
--- src/pages/api/subscriptions/contents.ts
+++ src/pages/api/subscriptions/contents.ts
@@ -1,22 +1,22 @@
 import {NextApiResponse} from 'next';
-import {authenticate} from '@/middleware/authMiddleware';
 import {AuthenticatedRequest} from "@/interfaces/auth.interface";
 import {subscriptionService} from "@/services/containers";
 import {errorHandler} from "@/middleware/errorHandlingMiddleware";
+import {authenticate} from "@/middleware/authMiddleware";
 
 async function handler(req: AuthenticatedRequest, res: NextApiResponse) {
     if (req.method !== 'GET') {
         throw {status: 405, message: "Method not allowed"};
     }
 
-    authenticate(req, res, async () => {
-        const userId = req.user?.userId
-        if (!userId) {
-            throw {status: 401, message: "Unauthorized: user not found"};
-        }
-        const {status, data} = await subscriptionService.getSubscribedContent(userId);
-        return res.status(status).json({success: true, data: data});
-    });
+    const userId = req.user?.userId
+    if (!userId) {
+        throw {status: 401, message: "Unauthorized: user not found"};
+    }
+    const {status, data} = await subscriptionService.getSubscribedContent(userId);
+    return res.status(status).json({success: true, data: data});
+
 
 }
-export default errorHandler(handler)
+
+export default errorHandler(authenticate(handler))
