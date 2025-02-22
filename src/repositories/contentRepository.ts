c18a6147cf832ea9b46e67abe7a8ed585c6d42be
Satta Sundar Talukdar <bappu.ctg@gmail.com>
2025-02-22 18:26:18 +0600

    feat: add query to fetch latest 3 contents

--- src/repositories/contentRepository.ts
+++ src/repositories/contentRepository.ts
@@ -17,4 +17,27 @@ export class ContentRepository {
         return this.prisma.content.findMany({where: {categoryId: {in : categoryIds}}});
     }
 
+    /**
+     * Find max 3 contents of last week for weekly digest.
+     * @param categoryIds - list of category ids.
+     * @returns The list of contents of
+     */
+    async getLastWeekContentByCategories(categoryIds: string[]){
+        if (!categoryIds || categoryIds.length === 0) {
+            return []; //Return an empty array instead of querying this.prisma
+        }
+
+        return this.prisma.content.findMany({
+            where: {
+                categoryId: { in: categoryIds },
+                createdAt: { gte: new Date(Date.now() - 7 * 24 * 60 * 60 * 1000) }, // Last 7 days
+            },
+            orderBy: { createdAt: "desc" },
+            take: 3, // Max 3 articles per category
+            select: {
+                title: true,  // Only fetch the title field
+            },
+        });
+    }
+
 };
